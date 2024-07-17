41a5d6e297f41569b64b4a1225e97baaff638387
Sagar Golhar <sagar.golhar@logicmonitor.com>
2024-07-17 16:18:04 +0530

    Initializing the project

---
+++ src/test/java/com/logicmonitor/Alerts/AlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logicmonitor.Alerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
